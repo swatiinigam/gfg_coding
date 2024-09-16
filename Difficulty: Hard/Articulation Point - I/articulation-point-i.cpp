4b7e96f3434cddb906f7982cf2c110bc8d93ada4
Swati Nigam <95766340+swatiinigam@users.noreply.github.com>
2024-09-16 12:30:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Articulation Point - I/articulation-point-i.cpp
+++ Difficulty: Hard/Articulation Point - I/articulation-point-i.cpp
@@ -7,66 +7,67 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
+
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
 class Solution {
-public:
-    void APUtil(int u, vector<int> adj[], vector<bool>& visited, vector<int>& disc, vector<int>& low, vector<int>& parent, vector<bool>& ap) {
-        static int time = 0;
-        int children = 0;
-
-        visited[u] = true;
-        disc[u] = low[u] = ++time;
-
-        for (auto v : adj[u]) {
-            if (!visited[v]) {
-                children++;
-                parent[v] = u;
-                APUtil(v, adj, visited, disc, low, parent, ap);
-
-                low[u] = min(low[u], low[v]);
-
-                if (parent[u] == -1 && children > 1) {
-                    ap[u] = true;
-                }
-
-                if (parent[u] != -1 && low[v] >= disc[u]) {
-                    ap[u] = true;
-                }
-            } else if (v != parent[u]) {
-                low[u] = min(low[u], disc[v]);
+  public:
+  int timer=0;
+    void dfs(vector<int> &low,vector<int> &time,int node,int parent,vector<int> adj[],vector<int> &vis,set<int>&answer)
+    {
+         vis[node]=1;
+         timer++;
+         time[node]=timer;
+         low[node]=timer;
+         int child=0;
+         for(auto x:adj[node])
+         {
+            if(x==parent)
+            continue;
+            if(!vis[x])
+            {
+                 dfs(low,time,x,node,adj,vis,answer);
+                 low[node]=min(low[x],low[node]);
+                 if(low[x]>=time[node] && parent!=-1)
+                 {
+                  answer.insert(node);  
+                 }
+                 child++;
             }
-        }
+            else
+            {
+                low[node]=min(low[node],time[x]);
+            }
+         }
+         if(parent==-1 && child>1)
+         answer.insert(node);
     }
-
-    vector<int> articulationPoints(int V, vector<int> adj[]) {
-        vector<bool> visited(V, false);
-        vector<int> disc(V, -1);
-        vector<int> low(V, -1);
-        vector<int> parent(V, -1);
-        vector<bool> ap(V, false);
-
-        for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                APUtil(i, adj, visited, disc, low, parent, ap);
+    vector<int> articulationPoints(int n, vector<int>adj[]) {
+        vector<int> time(n,-1);
+        vector<int> low(n,-1);
+        vector<int> vis(n,0);
+        vector<int> ans;
+        set<int> s;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                dfs(low,time,i,-1,adj,vis,s);
             }
         }
-
-        vector<int> result;
-        for (int i = 0; i < V; i++) {
-            if (ap[i]) {
-                result.push_back(i);
-            }
+        if(s.size()==0)
+        return {-1};
+        for(auto x:s)
+        {
+            ans.push_back(x);
         }
-
-        if (result.empty()) {
-            return {-1};
-        }
-        
-        return result;
+      
+        return ans;
+        // Code here
     }
 };
 
