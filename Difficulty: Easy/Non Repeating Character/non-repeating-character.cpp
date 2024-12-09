8650c717615d43d8e17c14a944d913ec4115e5f1
Swati Nigam <95766340+swatiinigam@users.noreply.github.com>
2024-12-09 21:07:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the first non-repeating character in a string.
+    char nonRepeatingChar(string &s) {
+        // Your code here
+            int freq[26] = {0};
+    
+            for(int i = 0; i < s.size() ; i++) {
+                freq[s[i] - 'a']++;
+            }
+    
+    
+            for(int i = 0; i < s.size() ; i++) {
+                
+                if(freq[s[i] - 'a'] == 1) {  
+                    return s[i];
+                }
+            }
+    
+            return '$';
+        }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+
+        string S;
+        cin >> S;
+        Solution obj;
+        char ans = obj.nonRepeatingChar(S);
+
+        if (ans != '$')
+            cout << ans;
+        else
+            cout << "-1";
+
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
