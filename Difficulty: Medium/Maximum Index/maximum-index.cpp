3c7d78cfcbe02e4e53af58d312afe5aa9b52e96e
Swati Nigam <95766340+swatiinigam@users.noreply.github.com>
2024-12-10 13:53:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Index/maximum-index.cpp
+++ Difficulty: Medium/Maximum Index/maximum-index.cpp
@@ -1,48 +1,64 @@
 //{ Driver Code Starts
 #include <bits/stdc++.h>
-
 using namespace std;
 
-// } Driver Code Ends
 
-class Solution{
-public:
-    int maxIndexDiff(int arr[], int n) {
-        // code here
-        stack<int>st;
-        for(int i=n-1;i>=0;i--){
-            if(st.empty() or arr[st.top()]<arr[i])
-                st.push(i);
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(!st.empty() and arr[i]<=arr[st.top()]){
-                ans = max(ans,st.top()-i);
-                st.pop();
-                i--;
-            }
-        }
-        return ans;
+// } Driver Code Ends
+class Solution {
+  public:
+    // arr[]: input array
+    // Function to find the maximum index difference.
+   int maxIndexDiff(vector<int>& arr) {
+      int n = arr.size();
+      vector<int>leftarray;
+      vector<int>rightarray(n);
+      int mini = INT_MAX;
+      for(int i=0; i<n; i++){
+          mini = min(mini, arr[i]);
+          leftarray.push_back(mini);
+      }
+     int maxi = INT_MIN;
+      for(int i=n-1; i>=0; i--){
+          maxi = max(maxi, arr[i]);
+          rightarray[i] = maxi;
+      }
+      int i=0;
+      int j = 0;
+      int ans = 0;
+      while(i<n && j<n){
+          if(leftarray[i]<=rightarray[j]){
+              ans = max(ans, j-i);
+              j++;
+          }
+          else{
+              i++;
+          }
+      }
+      return ans;
     }
 };
-
 
 //{ Driver Code Starts.
 
 int main() {
-    int t;
-    cin >> t;
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
     while (t--) {
-        int n, i;
-        cin >> n;
-        int a[n];
-        for (i = 0; i < n; i++) {
-            cin >> a[i];
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
         }
         Solution ob;
-        auto ans = ob.maxIndexDiff(a, n);
-        cout << ans << "\n";
+
+        // calling maxIndexDiff() function
+        cout << ob.maxIndexDiff(nums) << endl;
     }
+
     return 0;
 }
 // } Driver Code Ends
