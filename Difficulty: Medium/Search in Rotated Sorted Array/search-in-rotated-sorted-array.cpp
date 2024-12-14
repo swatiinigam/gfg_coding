862411c9205167e48046367907bc455f2ab377ac
Swati Nigam <95766340+swatiinigam@users.noreply.github.com>
2024-12-14 11:12:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -7,19 +7,58 @@ using namespace std;
 
 class Solution {
   public:
+   int pivotIndex(vector<int> arr)
+    {
+        int l=0,h=arr.size()-1;
+        int ans=-1;
+        
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            
+            if(arr[mid] < arr[mid+1] and arr[mid] < arr[mid-1]) return mid;
+            else if(arr[mid] > arr[h]) l=mid+1;
+            else
+                h=mid-1;
+        }
+        
+        return ans;
+    }
+    
+    int binarySearch(vector<int>& arr, int key,int l,int h)
+    {
+        //int l=0,h=arr.size();
+        int ans=-1;
+        
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            
+            if(arr[mid] == key) return mid;
+            else if(arr[mid] > key) h=mid-1;
+            else
+                l=mid+1;
+        }
+        
+        return ans;
+    }
     int search(vector<int>& arr, int key) {
         // complete the function here
-        int indx=-1;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]==key){
-                indx=i;
-                break;
-            }
+        
+        int rotated_ind=pivotIndex(arr);
+        int ind;
+        //cout<<"rotated_ind:"<<rotated_ind<<'\n';
+        if(rotated_ind!=-1)
+        {
+            ind=binarySearch(arr,key,0,rotated_ind-1);
+            if(ind!=-1) return ind;
+            else
+                return binarySearch(arr,key,rotated_ind,arr.size()-1);
         }
-        return indx;
+        else
+        return binarySearch(arr,key,0,arr.size()-1);
     }
 };
-
 
 //{ Driver Code Starts.
 int main() {
@@ -39,6 +78,7 @@ int main() {
         cin >> key;
         Solution ob;
         cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
