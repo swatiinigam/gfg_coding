1055bccd17008328d5175ecee4e76fac7ea05240
Swati Nigam <95766340+swatiinigam@users.noreply.github.com>
2025-02-20 10:25:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Find median in a stream/find-median-in-a-stream.cpp
+++ Difficulty: Hard/Find median in a stream/find-median-in-a-stream.cpp
@@ -3,81 +3,61 @@
 using namespace std;
 
 
-
 // } Driver Code Ends
 
-class Solution
-{
-    public:
-    //Function to insert heap.
-    priority_queue<int>s;
-    priority_queue<int,vector<int>,greater<int>>g;
-    void insertHeap(int &x)
-    {
-        if(s.size()==0){
-            s.push(x);
-            return ;
-        }
-        if(s.size()>g.size()){
-            if(x<s.top()){
-                g.push(s.top());
-                s.pop();
-                s.push(x);
+class Solution {
+  public:
+    vector<double> getMedian(vector<int> &arr) {
+        // code here
+         priority_queue<int>leftmaxheap;
+        priority_queue<int,vector<int>,greater<int>>rightminheap;
+        vector<double>ans;
+        for(int i=0;i<arr.size();i++){
+            leftmaxheap.push(arr[i]);
+            
+            int ele=leftmaxheap.top();
+            rightminheap.push(ele);
+            leftmaxheap.pop();
+            
+            if(rightminheap.size()>leftmaxheap.size()){
+                int ele=rightminheap.top();
+                leftmaxheap.push(ele);
+                rightminheap.pop();
             }
-            else{
-                g.push(x);
-            }
+            double median;
+            if(leftmaxheap.size()!=rightminheap.size())
+            median=leftmaxheap.top();
+            else
+            median=(double)(leftmaxheap.top()+rightminheap.top())/2;
+            ans.push_back(median);
         }
-        else{
-            if(x<s.top()){
-                s.push(x);
-            }
-            else{
-                g.push(x);
-                s.push(g.top());
-                g.pop();
-            }
-        }
-    }
-    
-    //Function to balance heaps.
-    void balanceHeaps()
-    {
-        
-    }
-    
-    //Function to return Median.
-    double getMedian()
-    {
-        if(s.size()==g.size()){
-            return (double)(s.top()+g.top())/2.0;
-        }else{
-            return s.top();
-        }
+        return ans;
     }
 };
 
-
 //{ Driver Code Starts.
-
-int main()
-{
-    int n, x;
+int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
-    	Solution ob;
-    	cin >> n;
-    	for(int i = 1;i<= n; ++i)
-    	{
-    		cin >> x;
-    		ob.insertHeap(x);
-    	    cout << floor(ob.getMedian()) << endl;
-    	}
-    
-cout << "~" << "\n";
-}
-	return 0;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        vector<double> ans = ob.getMedian(nums);
+        cout << fixed << setprecision(1);
+        for (auto &i : ans)
+            cout << i << " ";
+        cout << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
 }
 // } Driver Code Ends
