9a4e4e799ca07bfcd10b924d8ba14ac0c258e9b2
Swati Nigam <95766340+swatiinigam@users.noreply.github.com>
2025-03-25 10:23:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Boolean Parenthesization/boolean-parenthesization.cpp
+++ Difficulty: Hard/Boolean Parenthesization/boolean-parenthesization.cpp
@@ -1,76 +1,62 @@
 //{ Driver Code Starts
-// Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-// User function Template for C++
 
-class Solution{
-    int mod = 1003;
-    int helper(int leftT, int rightT, int leftF, int rightF, char c, bool flag)
-    {
-        if(flag)
-        {
-            if(c == '|') return (leftT*rightT + leftT*rightF + leftF*rightT) % mod;
-            if(c == '&') return (leftT*rightT) % mod;
-            if(c == '^') return (leftT*rightF + leftF*rightT) % mod;
+// } Driver Code Ends
+
+// User function Template for C++
+class Solution {
+  public:
+    int countWays(string &s) {
+        const int n = (s.size() + 1) / 2;
+        
+        std::vector<std::vector<std::array<int, 2>>> dp(n, std::vector<std::array<int, 2>>(n));
+        for (int i = 0; i < n; ++i) {
+            dp[i][i][s[i * 2] == 'T'] = 1;
         }
         
-        if(c == '|') return (leftF*rightF) % mod;
-        if(c == '&') return (leftF*rightF + leftT*rightF + leftF*rightT) % mod;
-        if(c == '^') return (leftT*rightT + leftF*rightF) % mod;
-    }
-    
-    int solve(vector<vector<vector<int>>> &dp, int i, int j, string s, bool isTrue)
-    {
-        if(i == j)
-        {
-            if(isTrue) return s[i] == 'T';
-            return s[i] == 'F';
+        for (int len = 1; len < n; ++len) {
+            for (int r = len; r < n; ++r) {
+                const int l = r - len;
+
+                for (int k = l; k < r; ++k) {
+                    for (int i = 0; i < 2; ++i) {
+                        for (int j = 0; j < 2; ++j) {
+                            bool idx;
+                            if (s[k * 2 + 1] == '&') {
+                                idx = i & j;
+                            }
+                            else if (s[k * 2 + 1] == '|') {
+                                idx = i | j;
+                            }
+                            else {
+                                idx = i ^ j;
+                            }
+                            
+                            dp[l][r][idx] += dp[l][k][i] * dp[k + 1][r][j];
+                        }
+                    }
+                }
+            }
         }
         
-        if(dp[i][j][isTrue] != -1) return dp[i][j][isTrue];
-        
-        int ans = 0;
-        for(int k = i + 1; k < j; k += 2)
-        {
-            int leftT = solve(dp,i,k-1,s,true);
-            int rightT = solve(dp,k+1,j,s,true);
-            int leftF = solve(dp,i,k-1,s,false);
-            int rightF = solve(dp,k+1,j,s,false);
-            
-            
-            int temp = helper(leftT,rightT,leftF,rightF,s[k], isTrue);
-            ans = (ans + temp) % mod;
-        }
-        
-        return dp[i][j][isTrue] = ans;
-    }
-public:
-    int countWays(int n, string s){
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2,-1)));
-        return solve(dp,0,s.length()-1,s,true);
+        return dp[0][n - 1][1];
     }
 };
 
-//{ Driver Code Starts.
 
-int main(){
+//{ Driver Code Starts.
+int main() {
     int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
+    cin >> t;
+    while (t--) {
         string s;
-        cin>>s;
-        
+        cin >> s;
         Solution ob;
-        cout<<ob.countWays(n, s)<<"\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
+        int ans = ob.countWays(s);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
 }
 // } Driver Code Ends
