7d6678b538f57225e1f3bef4138887cef18531b9
Swati Nigam <95766340+swatiinigam@users.noreply.github.com>
2025-06-29 10:23:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Split Array Largest Sum/split-array-largest-sum.cpp
+++ Difficulty: Hard/Split Array Largest Sum/split-array-largest-sum.cpp
@@ -1,86 +1,37 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    bool isPossible(int arr[],int n,int k,int mid)
+    int func(vector<int>&arr,int mid)
     {
-        int count=1;
-        int sum=0;
+        int n=arr.size(),sum=0;
+        int k=1;
         for(int i=0;i<n;i++)
         {
-            if(arr[i]+sum<=mid)
-            {
-                sum+=arr[i];
-            }
+            if(sum+arr[i]>mid){
+            sum=arr[i];
+            k++;}
             else
-            {
-                count++;
-                if(count>k || arr[i]>mid)
-                {
-                    return false;
-                }
-                sum=arr[i];
-            }
-        }
-        return true;
-    }
-    int splitArray(int arr[] ,int N, int K) {
-        // code here
-        int mini=INT_MAX;
-        int sum=0;
-        for(int i=0;i<N;i++)
-        {
             sum+=arr[i];
-            mini=min(mini,arr[i]);
         }
-        int end=sum;
-        int start=mini;
+        return k;
+    }
+    int splitArray(vector<int>& arr, int k) 
+    {
+        int n=arr.size();
+        int start=*max_element(arr.begin(),arr.end());
+        int end=accumulate(arr.begin(),arr.end(),0);
         int ans=0;
-        int mid=start+(end-start)/2;
-        
         while(start<=end)
         {
-            if(isPossible(arr,N,K,mid))
+            int mid=(start+end)/2;
+            int count=func(arr,mid);
+            if(count<=k)
             {
                 ans=mid;
                 end=mid-1;
             }
             else
-            {
-                start=mid+1;
-            }
-            mid=start+(end-start)/2;
+            start=mid+1;
         }
         return ans;
     }
-
-
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N, K;
-        
-        cin>>N>>K;
-        int arr[N];
-        
-        for(int i=0 ; i<N ; i++)
-            cin>>arr[i];
-
-        Solution ob;
-        cout<<ob.splitArray(arr,N,K);
-        cout<<"\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
